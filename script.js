be81fa21b36093a0fcb89042c6d78c0893d7d69b
Puja Sharma <55825774+Puja-Sh@users.noreply.github.com>
2021-03-27 14:27:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-const labels = document.querySelectorAll('.form-control label')
+const labels = document.querySelectorAll('.form-control label');
+const form = document.getElementById('formSubmit');
+let name = document.getElementById('userName');
+let email = document.getElementById('userEmail');
+let number = document.getElementById('userNumber');
+let interest = document.getElementById('userInterest');
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        subject: interest.value,
+        message: number.value
+    }
+   
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success') {
+            alert('Email Sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('something went wrong')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData))
+})
+
+
+
+
+
+  
+
+// STYLING
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('')
-
-    
+        .join('')    
 })
 
 window.onscroll = () => {
-    const nav = document.querySelector('#navbar');
+    const nav = document.querySelector('.navbar-section');
     if(this.scrollY <= 10) {
-        nav.className = '';
+        nav.className = 'navbar-section';
     }  else {
-        nav.className = 'scroll';
+        nav.className = 'navbar-section scroll';
     } 
   };
 
@@ -22,11 +61,20 @@ window.onscroll = () => {
 const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
+const options = document.getElementsByClassName("nav-option");
 
 open_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.add('visible'))
+ 
 })
 
 close_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.remove('visible'))
+   
 })
+
+for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", () =>{
+        nav.forEach(nav_el => nav_el.classList.remove('visible'))
+    });
+  }
